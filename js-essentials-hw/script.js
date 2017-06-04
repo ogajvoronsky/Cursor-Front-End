df3b97b9ebb9c88ecb6531f489e3a27e6c8df465
Oleksiy Haivoronsky <ogajvoronsky@gmail.com>
2017-06-04 16:30:07 +0300

    Add part 2 JS essentials home work

--- js-essentials-hw/script.js
+++ js-essentials-hw/script.js
@@ -1,4 +1,12 @@
-var name, time, place, event
+var name, time, place, event;
+var curs_uah = 26;
+var curs_euro = 30;
+// var curs
+
+
+// $.getJSON("http://resources.finance.ua/ua/public/currency-cash.json", function(data){
+//     curs = JSON.parse(data);
+// });
 
 
 
@@ -10,3 +18,12 @@ function submit_form() {
   event=prompt("Please specify event:");
   console.log(name + ' has a ' + event + ' today at ' + time + ' somwhere in ' + place);
 } 
+
+
+function conv_dollar() {
+  cash = parseInt(prompt('Сума в доларах:') );
+  alert(cash.toString() + ' Dollars are equal to '+ (cash*curs_uah).toString() + ' grns' );  }
+
+function conv_euro() {
+  cash = parseInt(prompt('Сума в євро:') );
+  alert(cash.toString() + ' Euros are equal to '+ (cash*curs_euro).toString() + ' grns' ); }
